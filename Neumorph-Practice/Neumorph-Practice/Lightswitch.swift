08124f8a1e0ad2ea25be3124cda0cc8b8860e0c5
RichKummer <rckummer@gmail.com>
2020-08-10 17:21:06 -0400

    Neumorphic Interaction and Animation added

--- Neumorph-Practice/Neumorph-Practice/Lightswitch.swift
+++ Neumorph-Practice/Neumorph-Practice/Lightswitch.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct Lightswitch: View {
+    @State var Switch = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
         HStack {
-            LightButton()
+            LightButton(Switch: $Switch, viewState: $viewState, isDragging: $isDragging)
                 
             Spacer()
-            PlusButton()
+                .frame(width: isDragging ? 0 : 10)
+            PlusButton(isDragging: $isDragging)
         }
         .padding(.horizontal, 15)
     }
@@ -27,6 +32,11 @@ struct Lightswitch_Previews: PreviewProvider {
 
 //MARK: LightButton
 struct LightButton: View {
+    
+    @Binding var Switch: Bool
+    @Binding var viewState: CGSize
+    @Binding var isDragging: Bool
+    
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
@@ -44,36 +54,67 @@ struct LightButton: View {
                 VStack {
                     RoundedRectangle(cornerRadius: 15, style: .continuous)
                         .frame(width: 60, height: 38)
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                        .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.15), radius: 10, x: 10, y: 10)
-                        .shadow(color: Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 3, x: -5, y: -5)
+                        .foregroundColor(Switch ? Color(#colorLiteral(red: 0.4215929548, green: 0.4680481758, blue: 0.7006902825, alpha: 1)) : Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(Switch ? 0 : 0.15), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).opacity(Switch ? 0 : 1), radius: 3, x: -5, y: -5)
                 }
                 .frame(width: 76, height: 50)
-                .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                .background(Switch ? Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)).opacity(Switch ? 0 : 1) : Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                 .cornerRadius(18)
                 .padding(.horizontal, 4)
                 .padding(.top, 10)
+                .offset(y: Switch ? 50 : 0)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        Switch.toggle()
+                    }
+                }
                 
                 Spacer()
                 
-                Text("On")
+                Text(Switch ? "Off" : "On")
                     .font(.system(size: 17, weight: .medium, design: .default))
                     .foregroundColor(Color(#colorLiteral(red: 0.7857988944, green: 0.8164139397, blue: 0.9598672945, alpha: 1)))
                     .padding(.bottom, 20)
+                    .offset(y: Switch ? -50 : 0)
+                    .animation(.spring())
             }
             .frame(height: 120)
-            .background(Color(#colorLiteral(red: 0.1756659481, green: 0.2916717629, blue: 0.8352418664, alpha: 1)))
+            .background(Switch ? Color(#colorLiteral(red: 0.1353112999, green: 0.2217664245, blue: 0.6547231916, alpha: 1)).opacity(0.8)  : Color(#colorLiteral(red: 0.1756659481, green: 0.2916717629, blue: 0.8352418664, alpha: 1)))
             .cornerRadius(20)
             .padding(.trailing, 10)
         }
-        .frame(width: 230, height: 135)
+        .frame(width: isDragging ? 250 : 230, height: 135)
         .background(Color(#colorLiteral(red: 0.3163430578, green: 0.4367289846, blue: 1, alpha: 1)))
         .cornerRadius(30)
+        .animation(.spring())
+        
+        .offset(x: viewState.width, y: viewState.height)
+        
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.viewState = value.translation
+                    if self.viewState.width > 10 {
+                        self.isDragging = true
+                }
+            }
+            .onEnded { value in
+                if self.viewState.width < 10 {
+                    self.isDragging = false
+                }
+                
+                self.viewState = .zero
+            }
+        )
     }
 }
 
 //MARK: PlusButton
 struct PlusButton: View {
+    @Binding var isDragging: Bool
+    
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 15)
@@ -87,5 +128,7 @@ struct PlusButton: View {
                 .font(.system(size: 30))
                 .foregroundColor(Color(#colorLiteral(red: 0.8321105647, green: 0.8321105647, blue: 0.8321105647, alpha: 1)))
         }
+        .offset(x: isDragging ? 200 : 0)
+        .animation(.spring())
     }
 }
--- Neumorph-Practice/Neumorph-Practice/Neumorph_PracticeApp.swift
+++ Neumorph-Practice/Neumorph-Practice/Neumorph_PracticeApp.swift
@@ -7,8 +7,10 @@
 
 import SwiftUI
 
+@available(iOS 14.0, *)
 @main
 struct Neumorph_PracticeApp: App {
+    @available(iOS 14.0, *)
     var body: some Scene {
         WindowGroup {
             ContentView()
